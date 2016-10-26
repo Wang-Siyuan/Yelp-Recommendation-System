68e3121a2322fcc9313d447792aa8799823e982b
Siyuan Wang <wsylafa@gmail.com>
2016-10-26 18:25:44 -0400

    Added basic predictor set up, and implemented ridge regression model for predicting user reviews.

---
+++ review_predictor/review_predictor.py
@@ -0,0 +1,64 @@
+import utility as u
+from pprint import pprint
+import numpy as np
+import sys
+import itertools
+from sklearn import linear_model
+
+user_data = u.parse_data_set('data-set/yelp_academic_dataset_user.json')
+business_data = u.parse_data_set('data-set/yelp_academic_dataset_business.json')
+review_data = u.parse_data_set('data-set/yelp_academic_dataset_review.json')
+
+training_data_set_size = 20;
+feature_size = 11;
+X = np.zeros((training_data_set_size, feature_size));
+Y = np.zeros((training_data_set_size, 1));
+
+# user_data_sample = (user_data for user_data in range(1, 20))
+for i,user_data_entry in enumerate(itertools.islice(user_data,20)):
+	sys.stdout.write(str(i))
+	pprint(user_data_entry)
+	X[i,0] = user_data_entry['average_stars'];
+	X[i,1] = u.get_nullable_attribute(user_data_entry['compliments'],'cool');
+	X[i,2] = u.get_nullable_attribute(user_data_entry['compliments'],'hot');
+	X[i,3] = u.get_nullable_attribute(user_data_entry['compliments'],'more');
+	X[i,4] = u.get_nullable_attribute(user_data_entry['compliments'],'writer');
+	X[i,5] = user_data_entry['fans'];
+	X[i,6] = user_data_entry['review_count'];
+	X[i,7] = user_data_entry['votes']['cool'];
+	X[i,8] = user_data_entry['votes']['useful'];
+
+
+for i,business_data_entry in enumerate(itertools.islice(business_data,20)):
+	sys.stdout.write(str(i))
+	pprint(business_data_entry)
+
+	X[i,9] = u.get_nullable_attribute_with_str_2_int(business_data_entry['attributes'],'Accepts Credit Cards');
+	X[i,10] = u.get_nullable_attribute_with_expected_value(business_data_entry['attributes'], 'Alcohol', 'full-bar');
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+	# X[i,9] = str_2_int(business_data_entry['attributes']['Accepts Credit Cards'])
+
+	# X[i,11] = business_data_entry['review_count'];
+
+for i,review_data_entry in enumerate(itertools.islice(review_data,20)):
+	sys.stdout.write(str(i))
+	pprint(review_data_entry)
+	Y[i] = review_data_entry['stars'];
+
+pprint(X)
+pprint(Y)
+
+reg = linear_model.Ridge (alpha = .5);
+reg.fit (X, Y); 
+print(reg.coef_);
+print(reg.intercept_);
+
+# pprint(next(user_data))
+# pprint(next(business_data))
+# pprint(next(review_data))
--- review_predictor/utility.py
+++ review_predictor/utility.py
@@ -1,11 +1,33 @@
 import json
 import codecs
-from pprint import pprint
 
 
-def parse_data_set()
-    user_data = []
-    with codecs.open('data-set/yelp_academic_dataset_user.json','rU','utf-8') as f:
+def parse_data_set(file_path):
+    with codecs.open(file_path,'rU','utf-8') as f:
         for line in f:
            yield json.loads(line)
 
+
+def str_2_int(val):
+	return int(val == 'true');
+
+def get_nullable_attribute(attribute_dict, key):
+	if key in attribute_dict:
+		return attribute_dict[key];
+	else:
+		return 0;
+
+def get_nullable_attribute_with_str_2_int(attribute_dict, key):
+	if key in attribute_dict:
+		return str_2_int(attribute_dict[key]);
+	else:
+		return 0;
+
+def get_nullable_attribute_with_expected_value(attribute_dict, key, expected_value):
+	if key in attribute_dict:
+		if attribute_dict[key] == expected_value:
+			return 1;
+		else:
+			return 0;
+	else:
+		return 0;
